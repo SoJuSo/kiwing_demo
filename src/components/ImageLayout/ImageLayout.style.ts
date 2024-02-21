eaf9d7f981c6a44852fcadd6d8ad49989c3cc53e
SoJuSo <asdfg7123@gmail.com>
2024-02-22 04:10:35 +0900

    ✨feat : 페이지 설명을 위한 설명 요약 추가
    💄design : 설명 요약 레이아웃 생성

--- src/components/ImageLayout/ImageLayout.style.ts
+++ src/components/ImageLayout/ImageLayout.style.ts
@@ -14,13 +14,19 @@ const slideIn = keyframes`
 `;
 
 export const ImageContainer = styled.div<{ $delay: number }>`
+  position: relative;
   transform: translateY(20%);
   animation: ${slideIn} ${({ $delay }) => `${$delay}ms`} ease forwards;
-  width: 20rem;
+  width: 20%;
+  max-width: 300px;
+  z-index: 1;
 
   :hover {
     transition: all 0.3s ease;
-    transform: scale(1.1);
+    transform: scale(1.2);
+    @media (max-width: 1500px) {
+      transform: scale(1.3); /* 원하는 크기로 조정합니다. */
+    }
   }
   :not(:hover) {
     transition: all 0.3s ease;
@@ -28,8 +34,31 @@ export const ImageContainer = styled.div<{ $delay: number }>`
 `;
 
 export const ImageWrap = styled.img`
-  width: 20rem;
+  width: 100%;
   height: auto;
 
   object-fit: contain;
+`;
+
+export const ImageSummary = styled.div`
+  position: absolute;
+  z-index: -1;
+  box-sizing: border-box;
+  font-size: 1.3vw;
+  font-weight: bold;
+  padding: 1vw 0.1vw;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+
+  pointer-events: none;
+
+  div {
+    padding: 1vw;
+    box-shadow: 0.1rem 0.1rem 1rem 0.25rem rgba(55, 55, 55, 0.5);
+    background-clip: border-box;
+    background-color: #48da79;
+    color: #fefefe;
+    border-radius: 1rem;
+  }
 `;
--- src/components/ImageLayout/ImageLayout.tsx
+++ src/components/ImageLayout/ImageLayout.tsx
@@ -1,9 +1,12 @@
-import { ImageContainer, ImageWrap } from "./ImageLayout.style";
+import { ImageContainer, ImageSummary, ImageWrap } from "./ImageLayout.style";
 
 const ImageLayout = ({ src, delay, alt }: { src: string; delay: number; alt: string }) => {
   return (
     <ImageContainer $delay={delay}>
       <ImageWrap src={src} alt={alt} />
+      <ImageSummary>
+        <div>{alt}</div>
+      </ImageSummary>
     </ImageContainer>
   );
 };
