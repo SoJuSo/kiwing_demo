121d4d76588f5b7c1bbb8d344eef8905f2efa24b
SoJuSo <asdfg7123@gmail.com>
2024-02-22 04:10:07 +0900

    💄design : 전역 높이 설정

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -2,11 +2,20 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 html {
+    width: 100%;
     height:100%;
 }
  
 body {
+    width: 100%;
+    height: 100%;
     margin:0;
+}
+
+#root {
+    width: 100%;
+    height: 100%;
+    position: relative;
 }
  
 .bg {
