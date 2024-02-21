8b42629b676d491e99a08136fc0ac2d9f8824fff
SoJuSo <asdfg7123@gmail.com>
2024-02-21 18:58:33 +0900

    💄design : 배경 스타일링 코드 설정
    액티브한 배경을 구현하는 css 추가

---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+    height:100%;
+}
+ 
+body {
+    margin:0;
+}
+ 
+.bg {
+    animation:slide 10s ease-in-out infinite alternate;
+    background-image: linear-gradient(-60deg, #61bb46 50%, #009c4d 50%);
+    bottom:0;
+    left:-50%;
+    opacity:.5;
+    position:fixed;
+    right:-50%;
+    top:0;
+    z-index:-1;
+}
+ 
+.bg2 {
+    animation-direction:alternate-reverse;
+    animation-duration:6s;
+}
+ 
+.bg3 {
+    animation-duration:7s;
+}
+ 
+
+ 
+h1 {
+    font-family:monospace;
+}
+ 
+@keyframes slide {
+    0% {
+        transform:translateX(-25%);
+    }
+ 
+    100% {
+        transform:translateX(25%);
+    }
+}
+`;
