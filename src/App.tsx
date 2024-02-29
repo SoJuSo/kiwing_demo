32fb683683a819f303ccc61193c620848f0e5091
SoJuSo <asdfg7123@gmail.com>
2024-02-29 15:57:38 +0900

    🔧chore : 소스맵 제거, proxy 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { useEffect } from "react";
 import Footer from "./components/Footer";
 import MainPage from "./pages/MainPage.tsx/MainPage";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    fetch("/api/v1/tags")
+      .then((res) => res.json())
+      .then((resp) => console.log(resp));
+  });
+
   return (
     <>
       <MainPage />
       <Footer />
     </>
   );
-}
+};
 
 export default App;
